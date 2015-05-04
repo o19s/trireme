e8632ba9c4dccf565f00547aa9ac55400c94ea71
erussell <erussell@channeliq.com>
2015-05-04 14:51:25 -0500

    Force python 3 import behavior so it works in python 2

--- trireme/migrators/solr.py
+++ trireme/migrators/solr.py
@@ -1,3 +1,4 @@
+from __future__ import absolute_import
 from invoke import task
 from requests.auth import HTTPBasicAuth
 import requests
--- trireme/trireme.py
+++ trireme/trireme.py
@@ -1,3 +1,4 @@
+from __future__ import absolute_import
 import os
 from invoke import Collection, task
 from trireme.migrators import cassandra, solr
