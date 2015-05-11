a6c43967ef70206e00c3fa1e2628fa24c96e73cf
Christopher Bradford <cbradford@opensourceconnections.com>
2015-05-11 14:27:40 -0400

    Changed version number

--- setup.py
+++ setup.py
@@ -2,13 +2,13 @@ from setuptools import setup, find_packages  # Always prefer setuptools over dis
 
 setup(
     name='trireme',
-    version='1.0.0',
+    version='1.1.0',
 
     description='Migration tool providing support for Apache Cassandra, DataStax Enterprise Cassandra, & DataStax '
                 'Enterprise Solr.',
 
     url='https://github.com/o19s/trireme',
-    download_url='https://github.com/o19s/trireme/tarball/1.0.0',
+    download_url='https://github.com/o19s/trireme/tarball/1.1.0',
 
     author='Christopher Bradford',
     author_email='cbradford@opensourceconnections.com',
@@ -16,12 +16,13 @@ setup(
     license='BSD',
 
     classifiers=[
-        'Development Status :: 3 - Alpha',
+        'Development Status :: 4 - Beta',
 
         'Intended Audience :: Developers',
         'Topic :: Software Development :: Build Tools',
 
         'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 2',
     ],
 
     keywords='cassandra solr dse dsc migrations development',
